19845d23e8aa7a5bbb6492d5a40ccaa49419fe88
Shailesh Padekar <shailesh.padekar@gmail.com>
2020-01-18 12:43:41 +0530

    loggin removed

--- src/main/java/com/ibm/dataservices/DataserviceApp.java
+++ src/main/java/com/ibm/dataservices/DataserviceApp.java
@@ -1,7 +1,5 @@
 package com.ibm.dataservices;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
